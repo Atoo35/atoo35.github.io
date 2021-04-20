3a2578daf8d232bfe19b6d2aa1118d95b0aec139
Atharva Deshpande <adrooney322@gmail.com>
2021-04-20 14:45:12 +0530

    left and center pane ready

---
+++ assets/js/router.js
@@ -0,0 +1,13 @@
+function changeRoute(routeName){
+        $('#mainFrame').attr('src',`./${routeName}.html`)
+        $('.channels').children('div').each(function(){
+                // console.log($(this).first().attr("id"));
+                $(this).children().each(function(){
+                        if($(this).attr('id') == routeName)
+                                $(this).addClass('channel-selected')
+                        else
+                                $(this).removeClass('channel-selected') 
+                })
+        })
+        $(`#${routeName}`).addClass("channel-selected")
+}
